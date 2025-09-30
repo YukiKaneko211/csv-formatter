e4d2194e4660010ab7c8e95a08ce2773bc021f49
RexOnp211 <illuminate.kmkz@outlook.com>
2025-09-30 16:53:25 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const repoName = "https://YukiKaneko211.github.io/csv-formatter/"; // ← あなたのリポジトリ名に置き換える
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: "export",        // 静的出力モード
+  images: {
+    unoptimized: true,     // GH Pagesでは画像最適化不可
+  },
+  basePath: process.env.NODE_ENV === "production" ? `/${repoName}` : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? `/${repoName}/` : "",
 };
 
-export default nextConfig;
+module.exports = nextConfig;
